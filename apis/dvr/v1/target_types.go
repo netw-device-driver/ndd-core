49b17d6c3e970fb7bbeeeb65c9b0ce8fff49df50
henderiw <wim.henderickx@gmail.com>
2021-06-30 16:46:12 +0200

    added const prefixes iso hardcoded values

--- apis/dvr/v1/target_types.go
+++ apis/dvr/v1/target_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1
 
 const (
+	configmapJsonConfig      = "config.json"
 	LabelApplication         = "app"
 	LabelNetworkDeviceDriver = "ndd"
 	PrefixNetworkNode        = "ndd"
